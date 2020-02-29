76d0ed33e50998dc02f9529cb360a84a5a0bafe6
최철환 <hwanyhee@naver.com>
2020-02-29 10:56:03 +0900

    리팩토링 - Player, Counter 컴포넌트 분리

--- src/App.js
+++ src/App.js
@@ -1,48 +1,7 @@
 import React from 'react';
 import {Header} from './components/Header';
 import './App.css';
-
-const Player = (props) => {
-  return (
-    <div className="player">
-      <span className="player-name">
-        <button className="remove-player" onClick={() => props.removePlayer(props.id)}> x </button>
-        {props.name}
-      </span>
-      <Counter score={props.score}/>
-    </div>
-  );
-}
-
-class Counter extends React.Component {
-  // 클래스 바로 아래에 선언되는 변수는 속성이 된다.
-  constructor() {
-    super();
-    this.state = {
-      score: 0
-    }
-    // this.handleChange.bind(this);
-  }
-
-  handleChange = (delta) => {
-    console.log(this); // arrow function 에서의 this 는 lexical this가된다.
-    console.log('handleChange');
-    // this.state.score += 1; 값만 변경
-    // 값을 변경하고 UI도 렌더링할려면 반드시 setState로 변경.
-    this.setState(prevState => ({ score: prevState.score + delta })); // 비동기로 동작
-  }
-
-  // 이벤트 핸들러 오른쪽은 반드시 함수 선언문이 와야 한다.
-  render() {
-    return (
-      <div className="counter">
-        <button className="counter-action decrement" onClick={() => this.handleChange(-1)}> - </button>
-        <span className="counter-score">{this.state.score}</span>
-        <button className="counter-action increment" onClick={() => this.handleChange(1)}> + </button>
-      </div>
-    );
-  }
-}
+import {Player} from "./components/Player";
 
 class App extends React.Component {
   state = {
