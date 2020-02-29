8ab97781ef9bf3c59fc301e0aa2d6a094fc1d396
최철환 <hwanyhee@naver.com>
2020-02-29 11:45:35 +0900

    컴포넌트 통신 - 자식과 부모간의 통신 프로토타입 구현2, 파라메터 넘기기

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -22,9 +22,13 @@ export class Counter extends React.Component {
   render() {
     return (
       <div className="counter">
-        <button className="counter-action decrement" onClick={this.props.changeScore}> - </button>
-        <span className="counter-score">{this.props.score}</span>
-        <button className="counter-action increment" onClick={this.props.changeScore}> + </button>
+        <div className="counter">
+          <button className="counter-action decrement"
+                  onClick={() => this.props.changeScore(this.props.id, -1)}> - </button>
+          <span className="counter-score">{this.props.score}</span>
+          <button className="counter-action increment"
+                  onClick={() => this.props.changeScore(this.props.id, 1)}> + </button>
+        </div>
       </div>
     );
   }
