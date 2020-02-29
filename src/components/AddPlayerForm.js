2245188f22a0c64a7eb65a8080f73db3b6a9c9b1
최철환 <hwanyhee@naver.com>
2020-02-29 13:14:34 +0900

    input을  controlled 컴포넌트로 변환

--- src/components/AddPlayerForm.js
+++ src/components/AddPlayerForm.js
@@ -1,10 +1,17 @@
 import React from 'react';
 
 export class AddPlayerForm extends React.Component {
+  state = {
+    value: ''
+  }
+  handleValueChane = (e) => {
+    this.setState({value: e.target.value});
+  }
   render() {
     return (
       <form action="" className="form">
-        <input type="text" className="input" placeholder="enter a player name" />
+        <input value={this.state.value} onChange={this.handleValueChane}
+               type="text" className="input" placeholder="enter a player name" />
         <input type="submit" className="input" value="Add Player" />
       </form>
     );
